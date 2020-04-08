2937f9fa066c6fc454da8aea7c283e971e80366f
Hamill210 <han95210@naver.com>
2020-04-08 14:09:09 +0900

    [BE] feat : Controller에 애노테이션 추가
    - RestController 애노테이션 추가
    - RequestMapping 애노테이션 추가
    
    associate with #5

--- server/src/main/java/com/codesquad/server/repository/ColumnRepository.java
+++ server/src/main/java/com/codesquad/server/repository/ColumnRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ColumnRepository extends CrudRepository<Column, Long> {
+
 }
--- server/src/main/java/com/codesquad/server/web/MainController.java
+++ server/src/main/java/com/codesquad/server/web/MainController.java
@@ -1,5 +1,14 @@
 package com.codesquad.server.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    @GetMapping("")
+    public String index() {
+        return "Hello World";
+    }
 
 }
