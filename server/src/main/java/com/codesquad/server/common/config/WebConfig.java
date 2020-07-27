d6bba02aca2df77328b1710cdfb05ad6685472fa
Ever <hsik0225@gmail.com>
2020-07-27 12:12:39 +0900

    [BE] refactor : interceptor 적용 해제

--- server/src/main/java/com/codesquad/server/common/config/WebConfig.java
+++ server/src/main/java/com/codesquad/server/common/config/WebConfig.java
@@ -1,25 +1,19 @@
 package com.codesquad.server.common.config;
 
-import com.codesquad.server.common.interceptor.JwtAuthInterceptor;
 import io.swagger.models.HttpMethod;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class WebConfig implements WebMvcConfigurer {
 
-    @NonNull
-    private final JwtAuthInterceptor jwtAuthInterceptor;
+//    @NonNull
+//    private final JwtAuthInterceptor jwtAuthInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -34,20 +28,20 @@ public class WebConfig implements WebMvcConfigurer {
                 );
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        List<String> swaggerPathPatterns = new ArrayList<>();
-
-        swaggerPathPatterns.add("/");
-        swaggerPathPatterns.add("/swagger-ui.html");
-        swaggerPathPatterns.add("/swagger-resources/**");
-        swaggerPathPatterns.add("/error");
-        swaggerPathPatterns.add("/webjars/**");
-        swaggerPathPatterns.add("/csrf");
-
-        registry.addInterceptor(jwtAuthInterceptor)
-                .addPathPatterns("/*")
-                .excludePathPatterns("/columns")
-                .excludePathPatterns(swaggerPathPatterns);
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        List<String> swaggerPathPatterns = new ArrayList<>();
+//
+//        swaggerPathPatterns.add("/");
+//        swaggerPathPatterns.add("/swagger-ui.html");
+//        swaggerPathPatterns.add("/swagger-resources/**");
+//        swaggerPathPatterns.add("/error");
+//        swaggerPathPatterns.add("/webjars/**");
+//        swaggerPathPatterns.add("/csrf");
+//
+//        registry.addInterceptor(jwtAuthInterceptor)
+//                .addPathPatterns("/*")
+//                .excludePathPatterns("/columns")
+//                .excludePathPatterns(swaggerPathPatterns);
+//    }
 }
